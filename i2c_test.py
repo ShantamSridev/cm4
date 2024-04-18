ac42636848d541296bf378ffccf10ca05d6b77e6
Shantam Sridev <50525078+ShantamSridev@users.noreply.github.com>
2024-04-18 02:06:05 +0100

    latest

--- i2c_test.py
+++ i2c_test.py
@@ -1,44 +1,72 @@
 import smbus
 import time
 import struct
+import i2c
+from i2c import bytes_to_float_array
+from i2c import send_i2c_command
+from i2c import request_i2c_command
+import RPi.GPIO as GPIO
 
 # Set up the I2C bus and slave address
 bus = smbus.SMBus(1)  # Check your bus number
-slave_address = 0x17  # Change to your slave's address
+slave_address = 0x23  # Change to your slave's address
 
+MOSFETPin = 12
 
-def send_i2c_command(bus, I2C_SLAVE_ADDRESS, mem_address, data):
-    # Convert the list of floats to a byte array
-    bytes_to_send = []
+GPIO.setmode(GPIO.BCM)   # Use physical pin numbering
+GPIO.setwarnings(False)
+GPIO.setup(MOSFETPin, GPIO.OUT, initial=GPIO.LOW)
 
-    for value in data:
-        # Convert each float to 4 bytes in little-endian format
-        float_bytes = struct.pack('=f',value)
-        # Extend the byte array with these bytes
-        bytes_to_send.extend(float_bytes)
+GPIO.output(MOSFETPin, GPIO.HIGH)
 
-    buf = bytes_to_send
-    print("Sent to",mem_address)
-    print(buf)
-    try:
-        bus.write_i2c_block_data(slave_address, mem_address, buf)
-    except OSError:
-        print("Couldn't write to slave, please check your wiring!")
+# while 1:
+#     dat = [20.1]
+#     mem_add = 0
+#     #send_i2c_command(bus, slave_address, mem_add, dat)
+#     request_i2c_command(bus, slave_address,mem_add)
+#     time.sleep(2)  # Wait a bit for the slave to process
 
-def request_i2c_command(bus, I2C_SLAVE_ADDRESS, mem_address):
+dat = [20.1]
+mem_add = 4 # set_get_angle
+send_i2c_command(bus, slave_address, mem_add, dat)
+time.sleep(5)  # Wait a bit for the slave to process
 
-    try:
-        block = bus.read_i2c_block_data(slave_address, mem_address, 32)
-    except OSError:
-        print("Couldn't read from slave")
-        
-    # Returned value is a list of 32 bytes
-    print(block)
+mem_add = 16 #set_angle
+dat = [180]
+send_i2c_command(bus, slave_address, mem_add, dat)
+time.sleep(5)
 
+mem_add = 20 #set_angle
+dat = [30]
+send_i2c_command(bus, slave_address, mem_add, dat)
+time.sleep(10)
 
-while 1:
-    dat = [20.1]
-    mem_add = 5
-    send_i2c_command(bus, slave_address, mem_add, dat)
-    request_i2c_command(bus, slave_address,mem_add)
-    time.sleep(2)  # Wait a bit for the slave to process
+mem_add = 20 #set_angle
+dat = [0]
+send_i2c_command(bus, slave_address, mem_add, dat)
+time.sleep(5)
+#mem_add = 8 #retreiving angle
+#request_i2c_command(bus, slave_address,mem_add)
+# time.sleep(5)  # Wait a bit for the slave to process
+
+# mem_add = 16 #set_angle
+# dat = [300]
+# send_i2c_command(bus, slave_address, mem_add, dat)
+# time.sleep(5)
+# mem_add = 8 #retreiving angle
+# request_i2c_command(bus, slave_address,mem_add)
+# time.sleep(2)  # Wait a bit for the slave to process
+# mem_add = 16 #set_angle
+# dat = [10]
+# send_i2c_command(bus, slave_address, mem_add, dat)
+# time.sleep(5)
+# mem_add = 8 #retreiving angle
+# request_i2c_command(bus, slave_address,mem_add)
+# time.sleep(2)  # Wait a bit for the slave to process
+# mem_add = 16 #set_angle
+# dat = [0]
+# send_i2c_command(bus, slave_address, mem_add, dat)
+# time.sleep(5)
+# mem_add = 8 #retreiving angle
+# request_i2c_command(bus, slave_address,mem_add)
+# time.sleep(2)  # Wait a bit for the slave to process
